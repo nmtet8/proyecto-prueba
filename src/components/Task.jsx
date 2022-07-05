13d02bd43d776f25a64c947c936801e42767a436
nmtet8 <41247646+nmtet8@users.noreply.github.com>
2022-07-05 14:59:02 -0300

    Update Task.jsx
    Comentarios

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,6 +3,14 @@ import { AiOutlineCloseCircle, AiOutlineHeart } from "react-icons/ai";
 import "../stylesheets/Task.css";
 
 function Task(props) {
+  
+  // El componente Task recibe en concreto 8 props: id (identificador que señala la posición de la tarea en el array "tasks"
+  // que es la variable de estado del componente TaskList), text (el texto ingresado en el input del componente TaskForm),
+  // completed (un boolean que asigna un clase que cambia el aspecto de una tarea cuando es marcada como completada),
+  // favourite (boolean que indica si una tarea ha sido marcada como favorito), deleteTask (funcion que elimina una tarea), 
+  // completeTask y markAsFavourite actualizan el estado de la propiedad props.completed y props.favourite
+  
+  
   const defineClassName = () => {
     if (props.completed && props.favourite) {
       return "task-container favourite completed";
